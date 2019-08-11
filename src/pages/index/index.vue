cf01a31d546791712bb4074f6873baae10348ea5
secrect47 <15228782447@163.com>
2019-08-11 21:46:14 +0800

    分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,144 +1,185 @@
 <template>
-	<div class="index">
-		<div class="head" style="background-image:url(/static/images/topHead.jpg);">
-			<div class="inp">
-				<input type="text" placeholder="搜索想要扔掉的垃圾">
-				<button>
-					<i class="iconfont">&#xe649;</i>
-				</button>
-			</div>
-		</div>
-		<div class="other">
-			<div class="hot">
-				<i class="iconfont">&#xe621;</i>
-			</div>
-			<div class="catalog">
-				<van-tree-select :items="items" :main-active-index="mainActiveIndex" :active-id="activeId"
-				 bind:click-nav="onClickNav" bind:click-item="onClickItem" />
-			</div>
-
-		</div>
-
-	</div>
-
+  <div class="index">
+    <div class="head" style="background-image:url(/static/images/topHead.jpg);">
+      <div class="inp">
+        <input type="text" placeholder="搜索想要扔掉的垃圾" />
+        <button>
+          <i class="iconfont">&#xe649;</i>
+        </button>
+      </div>
+    </div>
+    <div class="other">
+      <div class="hot">
+        <i class="iconfont">&#xe621;</i>
+      </div>
+      <div class="catalog">
+        <div class="tabs">
+          <ul class="tabUl">
+            <li
+              class="names"
+              v-bind:style="{width:itemWidth+'px',height:itemWidth+'px'}"
+              v-for="item in classify"
+              :key="item.id"
+            >
+              <img :src="item.imgUrl" />
+              <!-- {{item.name}} -->
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				searchText: "",
-				items: [{
+export default {
+  data() {
+    return {
+      searchText: "",
+      itemWidth: "",
+      classify: [
+        {
+          id: 1,
+          name: "可回收垃圾",
+          imgUrl: require("../../../static/images/kehuishouwu-middle.png"),
+          active: "active",
+          background: ""
+        },
+        {
+          id: 2,
+          name: "干垃圾",
+          imgUrl: require("../../../static/images/ganlaji-middle.png"),
+          active: ""
+        },
+        {
+          id: 3,
+          name: "湿垃圾",
+          imgUrl: require("../../../static/images/shilaji-middle.png"),
+          active: ""
+        },
+        {
+          id: 4,
+          name: "有害垃圾",
+          imgUrl: require("../../../static/images/youhailaji-middle.png"),
+          active: ""
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.setSize();
+  },
+  methods: {
+    serach(e) {
+      console.log(e, this.searchText);
+    },
+    setSize() {
+      let windowWidth = wx.getSystemInfoSync().windowWidth;
+      console.log(windowWidth);
+      let itemWidth = windowWidth * 0.9 / 4;
+      this.itemWidth = itemWidth;
+    }
+  },
 
-					// 导航名称
-					text: '',
-					// 禁用选项
-					disabled: false,
-					// 该导航下所有的可选项
-					children: [{
-							// 名称
-							text: '温州',
-							// id，作为匹配选中状态的标识
-							id: 1,
-							// 禁用选项
-							disabled: true
-						},
-						{
-							text: '杭州',
-							id: 2
-						}
-					]
-				}],
-			}
-		},
-		methods: {
-			serach(e) {
-				console.log(e, this.searchText)
-			}
-		},
-
-		created() {
-			// let app = getApp()
-		}
-	}
+  created() {
+    // let app = getApp()
+  }
+};
 </script>
 
 <style scoped>
-	@font-face {
-		font-family: 'iconfont';
-		/* project id 1308468 */
-		src: url('//at.alicdn.com/t/font_1308468_vxfgp658jo.eot');
-		src: url('//at.alicdn.com/t/font_1308468_vxfgp658jo.eot?#iefix') format('embedded-opentype'),
-			url('//at.alicdn.com/t/font_1308468_vxfgp658jo.woff2') format('woff2'),
-			url('//at.alicdn.com/t/font_1308468_vxfgp658jo.woff') format('woff'),
-			url('//at.alicdn.com/t/font_1308468_vxfgp658jo.ttf') format('truetype'),
-			url('//at.alicdn.com/t/font_1308468_vxfgp658jo.svg#iconfont') format('svg');
-	}
+@font-face {
+  font-family: "iconfont";
+  /* project id 1308468 */
+  src: url("//at.alicdn.com/t/font_1308468_vxfgp658jo.eot");
+  src: url("//at.alicdn.com/t/font_1308468_vxfgp658jo.eot?#iefix")
+      format("embedded-opentype"),
+    url("//at.alicdn.com/t/font_1308468_vxfgp658jo.woff2") format("woff2"),
+    url("//at.alicdn.com/t/font_1308468_vxfgp658jo.woff") format("woff"),
+    url("//at.alicdn.com/t/font_1308468_vxfgp658jo.ttf") format("truetype"),
+    url("//at.alicdn.com/t/font_1308468_vxfgp658jo.svg#iconfont") format("svg");
+}
 
-	.iconfont {
-		font-family: "iconfont" !important;
-		font-size: 16px;
-		font-style: normal;
-		-webkit-font-smoothing: antialiased;
-		-webkit-text-stroke-width: 0.2px;
-		-moz-osx-font-smoothing: grayscale;
-	}
+.iconfont {
+  font-family: "iconfont" !important;
+  font-size: 16px;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;
+}
 
+.head {
+  background: #dedede;
+  height: 140px;
+  position: relative;
+  background-size: 100% 100%;
+}
 
-	.head {
-		background: #dedede;
-		height: 140px;
-		position: relative;
-		background-size: 100% 100%;
-	}
+.inp {
+  position: absolute;
+  height: 50px;
+  bottom: -25px;
+  width: 90%;
+  left: 5%;
+  display: flex;
+  background: #ffffff;
+  border-radius: 5px;
+}
 
-	.inp {
-		position: absolute;
-		height: 50px;
-		bottom: -25px;
-		width: 90%;
-		left: 5%;
-		display: flex;
-		background: #FFFFFF;
-		border-radius: 5px;
-	}
+.inp input {
+  width: calc(100% - 50px);
+  height: 50px;
+  text-indent: 10px;
+  line-height: 50px;
+  padding-left: 10px;
+}
 
-	.inp input {
-		width: calc(100% - 50px);
-		height: 50px;
-		text-indent: 10px;
-		line-height: 50px;
-		;
-	}
+button::after {
+  border: 0;
+}
 
-	button::after {
-		border: 0;
-	}
+.inp button {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  background: transparent;
+}
 
-	.inp button {
-		width: 50px;
-		height: 50px;
-		line-height: 50px;
-		background: transparent;
-	}
+.van-search {
+  border-radius: 5px;
+}
 
-	.van-search {
-		border-radius: 5px;
-	}
+.imgs {
+  width: 100%;
+}
 
-	.imgs {
-		width: 100%;
-	}
+.other {
+  width: 90%;
+  margin: 0 auto;
+  margin-top: 30px;
+}
 
-
-	.other {
-		width: 90%;
-		margin: 0 auto;
-		margin-top: 30px;
-	}
-
-	.hot {
-		height: 50px;
-		line-height: 50px;
-	}
+.hot {
+  height: 50px;
+  line-height: 50px;
+}
+.tabUl {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  background: #ffffff;
+}
+.tabUl li {
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+}
+.tabUl li img {
+  width: 90%;
+  height: 90%;
+}
+.tabUl li.active {
+  border-bottom: #000;
+}
 </style>
